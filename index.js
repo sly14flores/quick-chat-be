06fdec76cde18350b063dcd8394b46e5d00ee551
Sly Flores <sly@christian.com.ph>
2021-10-12 10:50:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Router = require("./routes")
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 80
-const socket_port = process.env.SOCKET_PORT || 5000
+const socket_port = process.env.SOCKET_PORT || 80
 
 const { createServer } = require("http");
 const { Server } = require("socket.io");
